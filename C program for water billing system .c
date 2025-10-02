be30f291020b903683746702aec46f8b7130d830
LaurenceOduor <oduorlaurence459@gmail.com>
2025-10-02 13:29:38 +0300

    Add files via upload

---
+++ C program for water billing system .c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+ 
+ int units;
+ float totalbill;
+ 
+ printf("Enter units consumed: ");
+ scanf("%d",& units);
+ 
+ if (units <=30){
+ totalbill=units*20;
+ }
+ else if (units <=60){
+ totalbill =(30*20)+((units - 30)*25);
+ }
+ else{
+ totalbill =(30*20)+(30*25)+((units -60)*30);
+ }
+ printf("Total water bii: %.2fKES\n", totalbill); 
+ 
+ return 0;
+ 
+ }
